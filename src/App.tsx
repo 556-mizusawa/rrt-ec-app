64a033201f450302730628277372c7b64da44f65
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-04-16 17:14:00 +0900

    style & reset css作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./assets/reset.css";
+import "./assets/style.css";
 
-function App() {
-  return <></>;
-}
+const App: React.FC<{}> = () => {
+  return <>Hello</>;
+};
 
 export default App;
