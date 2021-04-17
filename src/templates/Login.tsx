f00684e3935b838cde953364fe6bd27d5d860087
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-04-17 17:21:11 +0900

    Router設定

--- src/templates/Login.tsx
+++ src/templates/Login.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { push } from "connected-react-router";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>ログイン</h2>
+      <button onClick={() => dispatch(push("/"))}>ログイン</button>
+    </div>
+  );
 };
 
 export default Login;
