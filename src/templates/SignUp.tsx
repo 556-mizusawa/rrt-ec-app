c6e9fece61e7034a165bb9635be4b5d2c89976ee
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-04-19 23:16:30 +0900

    パスワード確認欄変更

--- src/templates/SignUp.tsx
+++ src/templates/SignUp.tsx
@@ -1,3 +1,9 @@
+import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import InputLabel from "@material-ui/core/InputLabel";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 import React, { useCallback, useState } from "react";
 import { TextInput } from "../components/UIkit";
 
@@ -5,41 +11,53 @@ const SignUp: React.FC = () => {
   const [username, setUsername] = useState<string>(""),
     [email, setEmail] = useState<string>(""),
     [password, setPassword] = useState<string>(""),
-    [confirmPassword, setConfirmPassword] = useState<string>("");
+    [confirmPassword, setConfirmPassword] = useState<string>(""),
+    [showPassword, setShowPassword] = useState<boolean>(false);
 
   const inputUsername = useCallback(
-    (event) => {
+    (event: React.ChangeEvent<HTMLInputElement>) => {
       setUsername(event.target.value);
     },
     [setUsername]
   );
 
   const inputEmail = useCallback(
-    (event) => {
+    (event: React.ChangeEvent<HTMLInputElement>) => {
       setEmail(event.target.value);
     },
     [setEmail]
   );
 
   const inputPassword = useCallback(
-    (event) => {
+    (event: React.ChangeEvent<HTMLInputElement>) => {
       setPassword(event.target.value);
     },
-    [setPassword]
+    []
   );
 
   const inputConfirmPassword = useCallback(
-    (event) => {
+    (event: React.ChangeEvent<HTMLInputElement>) => {
       setConfirmPassword(event.target.value);
     },
-    [setConfirmPassword]
+    []
   );
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
 
   return (
     <div className="c-section-container">
       <h2 className="u-text__headline u-text-center">アカウント登録</h2>
       <div className="module-spacer--medium" />
       <TextInput
+        autoFocus={true}
         fullWidth={true}
         label={"ユーザー名"}
         multiline={false}
@@ -66,19 +84,32 @@ const SignUp: React.FC = () => {
         required={true}
         rows={1}
         value={password}
-        type={"password"}
+        type={showPassword ? "text" : "password"}
         onChange={inputPassword}
       />
-      <TextInput
-        fullWidth={true}
-        label={"パスワード(再確認)"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={confirmPassword}
-        type={"password"}
-        onChange={inputConfirmPassword}
-      />
+      <FormControl margin="dense" fullWidth={true}>
+        <InputLabel htmlFor="standard-adornment-password">
+          パスワード(確認用)
+        </InputLabel>
+        <Input
+          id="standard-adornment-password"
+          type={showPassword ? "text" : "password"}
+          required={true}
+          value={confirmPassword}
+          onChange={inputConfirmPassword}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
     </div>
   );
 };
