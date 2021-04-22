382052d72ab5b76329b7a01fc707cd85ed451ca8
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-04-23 00:09:40 +0900

    Products/ImagePreview 画像プレビュー機能追加

---
+++ src/components/Products/ImagePreview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { imagesProps } from "./type";
+
+const ImagePreview = (props: imagesProps) => {
+  return (
+    <div className="p-media__thumb" onClick={() => props.delete(props.id)}>
+      <img src={props.path} alt="商品画像" />
+    </div>
+  );
+};
+
+export default ImagePreview;
