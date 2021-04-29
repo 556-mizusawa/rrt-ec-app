25ad11903199ebc13773ce26198359556af1c47e
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-04-29 14:40:02 +0900

    Components/Header 作成

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import logo from "../../assets/img/icons/logo.png";
+import { useDispatch, useSelector } from "react-redux";
+import { getIsSignedIn } from "../../reducks/users/selectors";
+import { initialStateUsersType } from "../../reducks/store/type";
+import { push } from "connected-react-router";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuBar: {
+    backgroundColor: "#fff",
+    color: "#444",
+  },
+  toolBar: {
+    margin: "0 auto",
+    maxWidth: 1024,
+    width: "100%",
+  },
+  iconButtons: {
+    margin: "0 0 0 auto",
+  },
+});
+
+const Header: React.FC = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector(
+    (state: { users: initialStateUsersType }) => state
+  );
+  const isSignedIn = getIsSignedIn(selector);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.menuBar}>
+        <Toolbar className={classes.toolBar}>
+          <img
+            src={logo}
+            alt="logo"
+            width="128px"
+            onClick={() => dispatch(push("/"))}
+          />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
